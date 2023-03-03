537aa1989c285878bc95e07ce27d5b3c5559ae39
JunTeng1113 <JunTeng.1113@gmail.com>
2023-03-04 03:28:35 +0800

    Feat: 設計 Storybook 輸入欄

--- frontend/components/shared-component/textinput/textinput1/index.tsx
+++ frontend/components/shared-component/textinput/textinput1/index.tsx
@@ -4,8 +4,13 @@ const TextInput = styled.input`
   width: 100%;
   margin: 0.5rem 0;
   padding: 0.5rem;
-  border: 0.125rem solid rgba(0, 0, 0, 0.2);
-  border-radius: 0.25rem;
+  height: 1.2rem;
+  border-color: rgba(0, 0, 0, 0.5);
+  border-width: 0.075rem;
+  border-style: none none solid;
+  &:focus {
+    outline: none;
+  }
 `;
 
 export default TextInput;
