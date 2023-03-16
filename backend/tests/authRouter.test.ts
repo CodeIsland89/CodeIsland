70f27777e99cefbda89790ca30249e470c5656f5
agry88 <rgok307085@gmail.com>
2023-03-16 18:43:56 +0800

    Feat: add getUserProfile endpoint test

--- backend/tests/authRouter.test.ts
+++ backend/tests/authRouter.test.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import request from './index'
 import getRandomString from '../src/utils/getRandomString'
 
-describe('authRouter Test', () => {
+describe('userRelated Test', () => {
   const email = `${getRandomString(15)}@gmail.com`
   const password = 'this_is_my_password'
   const nickname = 'this_is_my_nickname'
@@ -26,11 +26,20 @@ describe('authRouter Test', () => {
     expect(response.status).toBe(200)
   })
 
+  let userToken = ''
   it('should Login', async () => {
     const response = await request.post('/api/auth/login').send({
       email,
       password
     })
+    userToken = response.body.token
+    expect(response.status).toBe(200)
+  })
+
+  it('should getUserProfile', async () => {
+    const response = await request
+      .post('/api/user/getUserProfile')
+      .set('Authorization', `Bearer ${userToken}`)
     expect(response.status).toBe(200)
   })
 })
