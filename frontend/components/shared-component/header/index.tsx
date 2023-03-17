053c8855fbca64b074c8a92fcc37978fcfe275f9
JunTeng1113 <JunTeng.1113@gmail.com>
2023-03-17 13:15:37 +0800

    Feat: Header初步架構

---
+++ frontend/components/shared-component/header/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import color from '../../../global/theme/color';
+
+const Header = styled.div`
+  width: 100%;
+  height: 3rem;
+  border-bottom-width: 0.05rem;
+  border-bottom-style: solid;
+  border-color: ${color.grey_400};
+  margin-bottom: 2rem;
+`;
+
+export default Header;
