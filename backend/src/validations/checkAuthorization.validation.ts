dba4300b24f3db31bb8cc29f28022576f3e17b33
agry88 <rgok307085@gmail.com>
2023-03-17 16:01:20 +0800

    Refactor: make check authorization a single function.

---
+++ backend/src/validations/checkAuthorization.validation.ts
@@ -0,0 +1,42 @@
+import { Ctx } from './../types/context'
+import { Request } from 'express'
+import findOneMember from '../services/findOneMember.service'
+import getDecodedToken from '../utils/getDecodedToken'
+import getErrorMessage from '../utils/getErrorMessage'
+import { Meta, Location } from 'express-validator'
+
+export default function checkAuthorization (ctx: Ctx): {
+  in: Location
+  notEmpty: {
+    errorMessage: string
+  }
+  custom: {
+    options: (_: any, { req }: Meta) => Promise<boolean>
+  }
+} {
+  return {
+    in: 'headers',
+    notEmpty: {
+      errorMessage: 'Token is required'
+    },
+    custom: {
+      options: async (_: any, { req }: Meta) => {
+        try {
+          const { prisma } = ctx
+          const { id: memberID } = getDecodedToken(req as Request)
+          const member = await findOneMember(prisma, {
+            member_id: memberID
+          })
+
+          req.locals = {
+            member
+          }
+
+          return true
+        } catch (error) {
+          throw new Error(getErrorMessage(error))
+        }
+      }
+    }
+  }
+}
--- backend/src/validations/endpoints/getUserProfile.validation.ts
+++ backend/src/validations/endpoints/getUserProfile.validation.ts
@@ -1,36 +1,9 @@
 import { Ctx } from './../../types/context'
 import { checkSchema, ValidationChain } from 'express-validator'
-import getErrorMessage from '../../utils/getErrorMessage'
-import getDecodedToken from '../../utils/getDecodedToken'
-import findOneMember from '../../services/findOneMember.service'
-import { Request } from 'express'
+import checkAuthorization from '../checkAuthorization.validation'
 
 export default function getUserProfileValidation (ctx: Ctx): ValidationChain[] {
   return checkSchema({
-    Authorization: {
-      in: 'headers',
-      notEmpty: {
-        errorMessage: 'Token is required'
-      },
-      custom: {
-        options: async (token, { req }) => {
-          try {
-            const { prisma } = ctx
-            const { id: memberID } = getDecodedToken(req as Request)
-            const member = await findOneMember(prisma, {
-              member_id: memberID
-            })
-
-            req.locals = {
-              member
-            }
-
-            return true
-          } catch (error) {
-            throw new Error(getErrorMessage(error))
-          }
-        }
-      }
-    }
+    Authorization: checkAuthorization(ctx)
   })
 }
