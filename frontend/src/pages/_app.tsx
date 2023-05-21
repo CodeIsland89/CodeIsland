a0d51d3ced0cb8c2b941b49903bde8ed6947f8a8
Agry88 <rgok307085@gmail.com>
2023-05-21 15:57:40 +0800

    Fix: tranform ico to svg to image matting

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" sizes="any" />
       </Head>
       <Component {...pageProps} />
     </>
