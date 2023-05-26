1187537af0c8f9d51e4577e4b27f61d669507f5b
agry88 <rgok307085@gmail.com>
2023-05-26 13:14:56 +0800

    Fix: prevent judge infinity loop program.

--- backend/src/services/sendJudge.service.ts
+++ backend/src/services/sendJudge.service.ts
@@ -20,10 +20,21 @@ export default async function sendJudge ({
   if (judgeURL === undefined) throw new Error('Judge URL not found')
   const fetchURL = `${judgeURL}/submissions/?base64_encoded=false&wait=true`
 
+  const abortController = new AbortController()
+
+  const timer = setTimeout(() => {
+    abortController.abort()
+    return new Error('Request timeout')
+  }, 5000)
+
   const response = await axios.post<AxiosResponseType>(fetchURL, {
     source_code: sourceCode,
     language_id: languageId
+  }, {
+    signal: abortController.signal
   })
+
+  clearTimeout(timer)
 
   return response
 }
