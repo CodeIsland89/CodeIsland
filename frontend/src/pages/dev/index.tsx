ee640173c92c84c04c41ba84959f1b684b206f92
JunTeng1113 <JunTeng.1113@gmail.com>
2023-06-04 20:26:36 +0800

    Feat: Add cloze question test version. path: "/dev"

---
+++ frontend/src/pages/dev/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import reactStringReplace from 'react-string-replace';
+
+const newLineRegex = /(\n)/g; // 換行代號
+const fieldRegex = /({})/g; // 欄位代號
+
+const question = 'const a = {} ; \n const b = {} ; \n const c = {} + {} ; \n'; // 題目
+const options = [1, 2, 3, 5, 4]; // 選項
+const correctAnswers = [1, 0, 3, 4]; // 正確答案
+
+export default function Index() {
+  const [selectedAnswers, setSelectedAnswers] = useState([]); // 選擇的答案
+
+  const handleSelect = (e) => {
+    const { name } = e.target;
+    if (selectedAnswers.includes(name)) {
+      setSelectedAnswers((prev) => prev.filter((answer) => answer !== name));
+    } else if (selectedAnswers.length < correctAnswers.length) { // 如果答案數量少於正確答案數量才能繼續選擇其他答案
+      setSelectedAnswers((prev) => [...prev, name]);
+    }
+  };
+
+  const Container = styled.div`
+    margin: 10rem;
+    border: 1px solid black;
+    padding: 1rem;
+    line-height: 2rem;
+    font-size: 1rem;
+    display: block;
+    height: max-content;
+
+    .selected {
+      background-color: #4CAF50;
+    }
+    
+    .selected:hover {
+      background-color: #3C9F40;
+    }
+  `;
+
+  const Field = styled.span`
+    display: inline-block;
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.5rem;
+    margin: 0.2rem 0.25rem;
+    min-width: 2rem;
+    line-height: 1.6rem;
+    height: 1.6rem;
+
+    text-align: center;
+    color: white; // 文字顏色
+    background-color: #4CAF50; // 背景顏色
+  `;
+
+  const Option = styled.button`
+  display: inline-block;
+  padding: 8px 16px;
+  margin: 4px;
+  border: none;
+  border-radius: 4px;
+  background-color: #ddd;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ccc;
+  }
+  `;
+
+  const Options = options.map((option, index) => (
+    <Option
+      key={option}
+      name={`op-${index}`}
+      onClick={(e) => handleSelect(e)}
+      className={
+        selectedAnswers.includes(`op-${index}`) ? 'selected' : ''
+      }
+    >
+      {option}
+    </Option>
+  ));
+
+  const clozeCode = () => {
+    let replacedCode = reactStringReplace(question, fieldRegex, (match, i) => {
+      const answer = selectedAnswers[Math.floor(i / 2)];
+      const option = Options.find((target) => target.props.name === answer);
+      return <Field key={i}>{option ? option.key : ''}</Field>;
+    });
+
+    replacedCode = reactStringReplace(replacedCode, newLineRegex, () => (
+      <br />
+    ));
+
+    return replacedCode;
+  };
+
+  return (
+    <>
+      {/* 題目 */}
+      <Container>
+        {clozeCode()}
+      </Container>
+      {/* 選項 */}
+      <Container>
+        {Options}
+      </Container>
+    </>
+  );
+}
